4b2bc842a97e9fbaf3dc154c78d6317fe97e8f9c
Yichen Ding <yichending01@gmail.com>
2024-09-21 23:22:24 -0400

    added check texture

--- material.h
+++ material.h
@@ -2,7 +2,7 @@
 #define MATERIAL_H
 
 #include "hittable.h"
-
+#include "texture.h"
 
 class material {
     public: 
@@ -16,7 +16,8 @@ class material {
 
 class lambertian : public material {
     public: 
-        lambertian(const color& albedo) : albedo(albedo) {}
+        lambertian(const color& albedo) : tex(make_shared<solid_color>(albedo)) {}
+        lambertian(shared_ptr<texture> tex) : tex(tex) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
             auto scatter_direction = rec.normal + random_unit_vector();
@@ -27,12 +28,12 @@ class lambertian : public material {
             }
 
             scattered = ray(rec.p, scatter_direction, r_in.time());
-            attenuation = albedo;
+            attenuation = tex->value(rec.u, rec.v, rec.p);
             return true;
         }
     
     private:
-        color albedo;
+        shared_ptr<texture> tex;
 };
 
 
